459082da6521787904d53b2e93cd82588cd71bc6
Jany Wu <janywu37@gmail.com>
2020-06-24 22:15:32 -0400

    R code

---
+++ plot1.R
@@ -0,0 +1,5 @@
+power<-read.table("household_power_consumption.txt",header=T,sep=";")
+subpower<-subset(power, power$Date=="1/2/2007"|power$Date=="2/2/2007")
+hist(as.numeric(as.character(subpower$Global_active_power)),col="red", main = "Global Active Power", xlab="Global Active Power(kilowatts)")
+title(main="Global Active Power")
+png("plot1.png", width=480, height=480)
